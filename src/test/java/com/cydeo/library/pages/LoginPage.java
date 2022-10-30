b8fe3298a41bd4fe182b3c70f6f4b7016c19a1cb
Gulsum Kaymaz <glsmkaymazz@gmail.com>
2022-10-30 14:15:57 +0300

    first commıt

---
+++ src/test/java/com/cydeo/library/pages/LoginPage.java
@@ -0,0 +1,21 @@
+package com.cydeo.library.pages;
+
+import com.cydeo.library.utilities.Driver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+
+    }
+
+
+
+
+
+
+
+
+}
